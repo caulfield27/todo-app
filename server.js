530317c981a2d363df881ad673ec93f6775fbd29
caulfield27 <al1onggwp@gmail.com>
2025-03-05 17:57:51 +0500

    update cron tasks handle

--- server.js
+++ server.js
@@ -1,43 +1,44 @@
 import { strapi } from "@/e_shared/api";
+import { BASE_URL } from "@/e_shared/get-env";
 import { apiUrl } from "@/routes";
 import axios from "axios";
 import cron from "node-cron";
 
-const intervals = new Map();
 //86400000
-
 export async function startCron(email, userId, token) {
-  const cronId = cron.schedule("8 16 * * *", () => {
-    try {
-      const intervalId = setInterval(() => {
-        console.log('sup');
-        
-        // axios.post('/api/checkTodoes',{email, userId, token}).then((res)=>{
-        //   console.log(res);
-        // }).catch((e)=>{
-        //   console.log('check todoes error: ', e);
-        // })
-      }, 5000);
-      intervals.set(email, intervalId);      
-      // strapi.post(apiUrl.cronId,payload,
-      //   {
-      //     headers: {
-      //       Authorization: `Bearer ${token}`,
-      //     },
-      //   }
-      // );
-      // cronId.stop();
-    } catch (e) {
-      console.log("cron err: ", e);
-    }
-  });
+  try {
+    const cronId = cron.schedule("* * * * *", () => {
+      console.log('test');
+      
+      // axios.post("/api/checkTodoes", {
+      //   email,
+      //   userId,
+      //   token,
+      // });
+    });
+
+    const strapiPayload = {
+      data: {
+        cronId: cronId.options.name,
+        user: email
+      },
+    };
+
+    fetch(`${BASE_URL}/api${apiUrl.cronId}`,{
+      method: "POST",
+      body: JSON.stringify(strapiPayload),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    }).catch((e)=>{
+      console.log('strapi cron post error: ', e);
+    })
+  } catch (e) {
+    console.log("start cron error: ", e);
+  }
 }
 
-
-export function stopCron(email){
-  console.log(intervals);
+export function stopCron(email) {
   
-  if(intervals.has(email)){
-    clearInterval(intervals.get(email));
-  }
 }
